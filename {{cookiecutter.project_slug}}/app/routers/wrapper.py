4f99b6ccc187bfcbe5d3d7c64e05460f74bea062
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2023-07-17 15:53:17 +0200

    Add login and call back urls

--- {{cookiecutter.project_slug}}/app/routers/wrapper.py
+++ {{cookiecutter.project_slug}}/app/routers/wrapper.py
@@ -1,8 +1,20 @@
 """
 Wrapper APIs. 
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from pydantic import BaseModel
+from requests_oauthlib import OAuth2Session
+from fastapi.responses import RedirectResponse
+import os
+
+HOSTNAME = os.environ.get("EXTERNAL_HOST_NAME")
+CLIENT_ID  = os.environ.get("CLIENT_ID")
+CLIENT_SECRET  = os.environ.get("CLIENT_SECRET ")
+CALLBACK_URL = os.environ.get("CALLBACK_URL ")
+SCOPES =["openid"]
+AUTH_URL = f"{HOSTNAME}/auth/realms/dome4.0/protocol/openid-connect/auth"
+TOKEN_URL = f"{HOSTNAME}/auth/realms/dome4.0/protocol/openid-connect/token"
+USER_INFO = f"{HOSTNAME}/auth/realms/dome4.0/protocol/openid-connect/userinfo"
 
 class CatalogData(BaseModel):
     """
@@ -31,6 +43,32 @@ async def home():
         A dictionary with a message key and "Hello World" value.
     """
     return {"msg": "Hello World"}
+
+@router.get("/login")
+async def login():
+    oauth_session = OAuth2Session(CLIENT_ID, redirect_uri=CALLBACK_URL, scope=SCOPES)
+    authorization_url, _ = oauth_session.authorization_url(AUTH_URL)
+    return RedirectResponse(authorization_url)
+
+@router.get("/callback")
+async def callback(request: Request):
+    scope_request = request.query_params.get("scope")
+    oauth_session = OAuth2Session(
+        CLIENT_ID, redirect_uri=CALLBACK_URL, scope=scope_request
+    )
+    try:
+        oauth_session.fetch_token(
+            TOKEN_URL,
+            client_secret=CLIENT_SECRET,
+            authorization_response=request.url._url,
+            scope=scope_request,
+            verify=False,
+        )
+    except Exception:
+        return request.query_params.get("error_description")
+    user = oauth_session.get(USER_INFO)
+
+    return user.content
 
 @router.post("/fetch-application-output-url")
 async def fetch_output_url(dome_catalog_data: CatalogData):
